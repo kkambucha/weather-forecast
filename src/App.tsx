66d87b356b24795e49e6d17ac383577522499929
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-10-26 21:49:05 +0300

    Add and config eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import './styles/index.scss';
+import React, { FC } from 'react'
+import logo from './assets/logo.svg'
+import './styles/index.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <p>Regular</p>
-      <p className="medium">Medium</p>
-      <p className="bold">Bold</p>
-      <p className="merriweather">Simple but powerful weather forcasting service based on OpenWeatherMap API</p>
-    </div>
-  );
+const App: FC = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <p>Regular</p>
+            <p className="medium">Medium</p>
+            <p className="bold">Bold</p>
+            <p className="merriweather">
+                Simple but powerful weather forcasting service based on
+                OpenWeatherMap API
+            </p>
+        </div>
+    )
 }
 
-export default App;
+export default App
