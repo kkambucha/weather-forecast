b1d941cbf624689af8b2cfb25d94ad258f469f55
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-10-27 21:07:41 +0300

    Reformat code with spaces instead tabs, add redux, add environment variables file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 test('renders learn react link', () => {
-    render(<App />)
-    const textElement = screen.getByText(/Regular/i)
-    expect(textElement).toBeInTheDocument()
+  render(<App />)
+  const textElement = screen.getByText(/Regular/i)
+  expect(textElement).toBeInTheDocument()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React, { FC } from 'react'
+
+import { Counter } from './features/counter/Counter'
 import logo from './assets/logo.svg'
 import './styles/index.scss'
 
 const App: FC = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
-            <p>Regular</p>
-            <p className="medium">Medium</p>
-            <p className="bold">Bold</p>
-            <p className="merriweather">
-                Simple but powerful weather forcasting service based on
-                OpenWeatherMap API
-            </p>
-        </div>
-    )
+  // console.log(process.env.REACT_APP_OPEN_WEATHER_API_KEY)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Counter />
+      <p>Regular</p>
+      <p className="medium">Medium</p>
+      <p className="bold">Bold</p>
+      <p className="merriweather">
+        Simple but powerful weather forcasting service based on OpenWeatherMap
+        API
+      </p>
+    </div>
+  )
 }
 
 export default App
