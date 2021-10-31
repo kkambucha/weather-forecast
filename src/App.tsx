74a6dee951063b2c3119c07a2ff1ea71334b836a
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-10-31 23:38:22 +0300

    City weather properties responsive margins

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,33 @@ import './App.scss'
 const App: FC = () => {
   return (
     <div className="App">
-      <header className="row">
-        <div className="col-1">
-          <div className="App_logo">
-            <Logo />
+      <header className="App_header">
+        <div className="row">
+          <div className="col-1">
+            <div className="App_logo">
+              <Logo />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1">
+            <h1 className="App_h1">Weather forecast</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <p className="App_p">
+              Simple but powerful weather forcasting service based on
+              OpenWeatherMap
+            </p>
+          </div>
+          <div className="col-4 pull-right pull-bottom">
+            <div className="App_search">
+              <Search />
+            </div>
           </div>
         </div>
       </header>
-      <div className="row">
-        <div className="col-1">
-          <h1 className="App_h1">Weather forecast</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-4">
-          <p className="App_p">
-            Simple but powerful weather forcasting service based on
-            OpenWeatherMap
-          </p>
-        </div>
-        <div className="col-4 pull-right pull-bottom">
-          <div className="App_search">
-            <Search />
-          </div>
-        </div>
-      </div>
       <Cities />
     </div>
   )
