b01664406c7e2e68d7b1a5f09a870daaf3fb712b
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-11-01 19:16:00 +0300

    Remove jest, add offline search cypress test-case

--- cypress/integration/App.spec.tsx
+++ cypress/integration/App.spec.tsx
@@ -1,6 +1,15 @@
 /// <reference types="cypress" />
 // @ts-check
+import { goOffline, goOnline } from '../utils/network'
 export {}
+
+const addLondonCityBySearch = (): void => {
+  cy.get('.Search_input').clear()
+  cy.get('.Search_input').type('london{enter}')
+  cy.get('.Search_button').click()
+  cy.get(':nth-child(1) > .SearchResultItem_button').click()
+}
+
 describe('basic integration test', () => {
   beforeEach(() => {
     cy.visit('/')
@@ -25,9 +34,7 @@ describe('basic search, add and delete city', () => {
   })
 
   it('find London by search input, add it and delete', () => {
-    cy.get('.Search_input').clear()
-    cy.get('.Search_input').type('london{enter}')
-    cy.get('.Search_button').click()
+    addLondonCityBySearch()
     cy.get('.SearchResult_list').should('exist')
     cy.get(
       ':nth-child(1) > .SearchResultItem_button > .SearchResultItem_buttonContent'
@@ -36,5 +43,36 @@ describe('basic search, add and delete city', () => {
     cy.get('.City_button').click()
     cy.get('.Cities .Cities_empty').should('exist')
     cy.get('.Cities').should('not.contain', 'London')
+  })
+})
+
+describe('get data from localstorage', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('save and get cities from localstorage', () => {
+    addLondonCityBySearch()
+    cy.reload()
+    cy.get('.Cities .City').should('exist')
+    cy.get('.City .City_name').should('be.visible')
+    cy.get('.City .City_name').should('have.text', 'London')
+  })
+})
+
+describe('error messages in offline mode', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  afterEach(() => {
+    goOnline()
+  })
+
+  it('shows connection error in search tooltip then lost connection', () => {
+    goOffline()
+    cy.get('.Search_input').clear()
+    cy.get('.Search_input').type('london{enter}')
+    cy.get('.Search_button').click()
+    cy.get('.SearchResult_textTitle').should('contain', 'Something went wrong')
   })
 })
