c114c613692b27cc797efa9b62172917ef3f9df2
Zakovryashin Vadim <zakovryashin.v@gmail.com>
2021-11-01 18:25:32 +0300

    Integration test of search, add and delete cities

--- cypress/integration/App.js
+++ cypress/integration/App.js
@@ -1,17 +1,39 @@
 /// <reference types="cypress" />
+// @ts-check
 describe('basic integration test', () => {
-  it('renders logo block', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('renders logo block', () => {
     cy.get('.Logo_image').should('exist')
   })
 
   it('renders search input', () => {
-    cy.visit('/')
     cy.get('.Search_input').should('exist')
   })
 
   it('renders cities container', () => {
-    cy.visit('/')
     cy.get('.Cities').should('exist')
+  })
+})
+
+describe('basic search, add and delete city', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('find London by search input, add it and delete', () => {
+    cy.get('.Search_input').clear()
+    cy.get('.Search_input').type('london{enter}')
+    cy.get('.Search_button').click()
+    cy.get('.SearchResult_list').should('exist')
+    cy.get(
+      ':nth-child(1) > .SearchResultItem_button > .SearchResultItem_buttonContent'
+    ).click()
+    cy.get('.Cities').contains('.City', 'London')
+    cy.get('.City_button').click()
+    cy.get('.Cities .Cities_empty').should('exist')
+    cy.get('.Cities').should('not.contain', 'London')
   })
 })
